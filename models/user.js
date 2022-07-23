95082faf7346c0406ffa9ac712cdb30a15d48990
Diego Rivera <93798677+Diegorivera1110@users.noreply.github.com>
2022-07-22 19:41:13 -0700

    completed user model

--- models/user.js
+++ models/user.js
@@ -1,2 +1,43 @@
 const { Schema, model } = require('mongoose');
-const dateFormat
+const dateFormat = require('../utils/dateForamt');
+
+const userSchema = new Schema({
+    userName: {
+        type: String,
+        unique: true,
+        require: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        unique: true,
+        require: true,
+        match: [/.+\@.+\..+/]
+    },
+    thoughts: [
+        {
+            type: Schema.Type.ObjectId,
+            red: 'thoughts'
+        }
+    ],
+    friends: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'user'
+        }
+    ]
+},
+{
+    toJSON: {
+        virtuals: true
+    },
+    id: false
+});
+
+userSchema.virtual('friendsCount').get(function() {
+    return this.friends.length;
+});
+
+const user = model('user', userSchema);\
+
+module.exports = user;
